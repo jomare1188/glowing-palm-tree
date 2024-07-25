01fd9fcc30383b09cd13cef33572d3249d86c1ad
jomare1188 <jomare1188@gmail.com>
2024-07-25 15:41:53 -0300

    scripts for data normalization

---
+++ code/RUV.r
@@ -0,0 +1,122 @@
+#BiocManager::install("RUVSeq")
+library("RUVSeq")
+library("DESeq2")
+library("tximport")
+library("tidyverse")
+library("wesanderson")
+
+# load data direct from deseq
+#txgen2="../data/sugarcane_tx2gen.txt"
+txgen2 = "../data/sorghum_tx2gen.txt"
+#metadata="../data/sugarcane_metadata.csv"
+metadata = "../data/sorghum_metadata.csv"
+
+
+# matrices
+#sorghum_raw <- read.table("../results/sorghum_raw_counts.tsv", header = T, row.names = 1, sep = "\t")
+#cane_raw <- t(read.table("../results/sugarcane_raw_counts.tsv", header = T, row.names = 1, sep = "\t"))
+# metadata
+#cane_metadata <- read.table("../data/sugarcane_metadata.csv", header = T,  sep = ",")
+sorghum_metadata <- read.table("../data/sorghum_metadata.csv", header = T,  sep = ",")
+
+sweet_genotypes <- c("BTx623", "BTx642", "RTx430") # for sorghum
+sorghum_metadata$group <- ifelse((sorghum_metadata$Cultivar %in% sweet_genotypes), "Sweet", "Grain")
+
+#sugar_genotypes <- c("SRA5", "SRA3", "Q238") # for cane
+#cane_metadata$group  <-  ifelse((cane_metadata$genotype %in% sugar_genotypes), "sugar", "fiber")
+
+
+sorghum_metadata_files = paste0("/home/dmpachon/jorge/comparative_cane/data/", pull(sorghum_metadata, "Cultivar"),"/3_salmon_quant/" ,pull(sorghum_metadata , "Run"), "/quant.sf")
+names(sorghum_metadata_files) = pull(sorghum_metadata, "Run")
+
+tx2gene = read.table(txgen2, sep = ",", col.names =c("transid","geneid"))
+
+# import count data to tximport
+count_data = tximport( files = sorghum_metadata_files,
+	type = "salmon",
+        tx2gene =  tx2gene,
+        txOut = T,
+        ignoreTxVersion = F)
+
+raw <- DESeqDataSetFromTximport(txi = count_data,
+        colData = sorghum_metadata,
+        design = ~ group)
+
+# filter not expressed genes
+dim(counts(raw))
+keep<- rowSums(counts(raw))>0
+table(keep)
+raw <- raw[keep,]
+print("datos raw filtrados")
+dim(raw)
+
+# RUVs
+cv_row <- function(row) {
+  sd(row) / mean(row) * 100
+}
+
+cv <- apply(counts(raw), 1, cv_row)
+negative_control <- names(head(cv[order(cv)],1000))
+differences <- makeGroups(cane_metadata$genotype)
+
+# RUVr
+# some magic
+design <- model.matrix(~sorghum_metadata$group)
+y <- DGEList(counts=counts(raw), group=sorghum_metadata$group)
+keep <- filterByExpr(y)
+y <- y[keep, , keep.lib.sizes=FALSE]
+y <- calcNormFactors(y, method="upperquartile")
+y <- estimateGLMCommonDisp(y, design)
+y <- estimateGLMTagwiseDisp(y, design)
+fit <- glmFit(y, design)
+res <- residuals(fit, type="deviance")
+
+# try for different k values
+set_RUVr <- RUVr(y$counts,rownames(y), k=1, res)
+dataRUVr <- DESeqDataSetFromMatrix(countData = set_RUVr$normalizedCounts,
+                                   colData = sorghum_metadata,
+                                   design = ~ group)
+
+RUVr_vst <- varianceStabilizingTransformation(dataRUVr)
+
+# plot PCA for RUVr(normalized) and vst(transformed) data
+colors = wes_palette("Darjeeling1",2 , type = "discrete")
+pca_data <- plotPCA(RUVr_vst, intgroup = c("group", "Cultivar"), returnData = TRUE)
+
+percentVar <- round(100 * attr(pca_data, "percentVar"))
+# plot R color condition
+colors = wes_palette("Darjeeling1",2 , type = "discrete")
+# Plooot 
+p <- ggplot(pca_data, aes(x = PC1, y = PC2, shape = as.factor(Cultivar),  color = as.factor(group.1)))
+p <- p + geom_point(size=3)
+p <- p + labs(title = "sorghum", color="Group", shape = "Genotype" )
+p <- p + xlab((paste0("PC1 : ", percentVar[1],"%")))
+p <- p + ylab((paste0("PC2 : ", percentVar[2],"%")))
+#p <- p + geom_text_repel(aes(label=name), size=2.5, max.overlaps = 20 )
+p <- p + scale_colour_manual(values = colors)
+#p <- p + scale_shape_manual(values = c(0, 1, 2, 3, 4, 5, 6, 8))
+p <- p + theme_bw()
+p <- p + theme(text = element_text(family = "Times New Roman", size=22),
+		panel.border = element_blank(),
+		panel.grid.major = element_blank(),
+		panel.grid.minor = element_blank(),
+		axis.line = element_line(colour = "black"))
+ggsave(p, filename = "../results/RUVr/sorghum/k1_RUVr_groups.png" ,units = "cm", width = 15*1.3, height = 15, dpi = 320, limitsize = F)
+
+#
+#	theme_bw() +
+#	geom_point(size=3.5, aes(shape = RUVr_vst$genotype)) +
+#	labs(title = "RUVr cane", col="Group", shape="Genotype") +
+#	scale_colour_manual(values = colors) +
+#	scale_shape_manual(values = c(0, 1, 2, 3, 4, 5, 6, 8))+
+#  	theme(text = element_text(size=22),
+#		panel.border = element_blank(),
+#		panel.grid.major = element_blank(),
+#		panel.grid.minor = element_blank(),
+#		plot.title = element_text(hjust = 0.5),
+#		axis.line = element_line(colour = "black"))
+#ggsave(ruvr_condition, filename = "../results/RUVr/cane/k2_RUVr_groups.png",units = "cm",width = 15*1.3, height = 15,dpi = 320)
+
+write.table(assay(dataRUVr), file = "../results/matrices/sorghum_RUVr_k1_raw_counts.tsv", sep = "\t" ,quote = F, col.names = T)
+write.table(assay(RUVr_vst), file = "../results/matrices/sorghum_RUVr_k1_vst_counts.tsv", sep = "\t" ,quote = F, col.names = T)
+
---
+++ code/pca_for_rnaseq.r
@@ -0,0 +1,96 @@
+## variables
+cores = 1
+# set working directory
+setwd(wd)
+
+## install packages
+# install.packages(c("BiocManager","backports","tidyverse"),
+# lib = "./libraries",
+# repos = 'http://cran.us.r-project.org')
+#BiocManager::install("tximport", lib = "./../libraries")
+#BiocManager::install("DESeq2", lib = "./../libraries")
+#BiocManager::install("BiocParallel", lib = "./../libraries")
+
+## load libraries 
+library(tximport)
+library(DESeq2)
+library(tidyverse)
+library(ggplot2)
+library(wesanderson)
+library(ggrepel)
+
+#txgen2="../data/sugarcane_tx2gen.txt"
+txgen2 = "../data/sorghum_tx2gen.txt"
+#metadata="../data/sugarcane_metadata.csv"
+metadata = "../data/sorghum_metadata.csv"
+
+get_full_trans_count_matrixes <- function(metadata, txgen2) {
+        sample_table <-read.table(metadata, sep = ",", header = T)
+	sugar_genotypes <- c("SRA5", "SRA3", "Q238")
+	fiber_genotypes <- c("KQB09-20432", "Q157", "Q151", "Q135")
+
+	grain_genotypes <- c("Rio", "Keller", "Della")
+	sweet_genotypes <- c("BTx623", "BTx642", "RTx430")
+
+	sample_table$group <- ifelse((sample_table$Cultivar %in% sweet_genotypes), "Sweet", "Grain")
+#	sample_table$group <- ifelse((sample_table$genotype %in% sugar_genotypes), "sugar", "fiber")
+	
+	separated_wider <- separate_wider_delim(sample_table , cols = ConditionDescription, delim = "_", names = c("time", "genotype2", "tissue"))
+	separated_wider$ConditionDescription <- sample_table$ConditionDescription
+	sample_table <- separated_wider
+
+        # load files paths
+#       sample_files = paste0("/home/dmpachon/jorge/comparative_cane/data/", pull(sample_table, "genotype"),"/3_salmon_quant/" ,pull(sample_table , "SampleID"), "/quant.sf")
+	sample_files = paste0("/home/dmpachon/jorge/comparative_cane/data/", pull(sample_table, "Cultivar"),"/3_salmon_quant/" ,pull(sample_table , "Run"), "/quant.sf")
+        # name table columns
+#       names(sample_files) = pull(sample_table, "SampleID")
+	names(sample_files) = pull(sample_table, "Run")
+        # relate genes to transcripts
+        tx2gene = read.table(txgen2, sep = ",", col.names =c("transid","geneid"))
+        # import count data to tximport
+        count_data = tximport( files = sample_files,
+                               type = "salmon",
+                               tx2gene =  tx2gene,
+                               txOut = T,
+                               ignoreTxVersion = F)
+
+        raw <- DESeqDataSetFromTximport(txi = count_data,
+                                        colData = sample_table,
+                                        design = ~ group)
+        # full raw and normalized count matrixes for all experiments
+        data <- estimateSizeFactors(raw)
+        vst <- varianceStabilizingTransformation(data)
+        df_data <- assay(raw)
+        df_vst <- assay(vst)
+        write.table(df_data, file = paste0("../results/", "sorghum_raw_counts.tsv", sep = ""), sep = "\t" ,quote = F, col.names = T)
+        write.table(df_vst, file = paste0("../results/", "sorghum_vst_counts.tsv", sep = ""), sep = "\t" ,quote = F, col.names = T)
+	# Make PCA with DESEQ2 function
+#	pca_data <- plotPCA(vst, intgroup = c("group", "genotype", "tissue", "ConditionDescription", "time"), returnData = TRUE)
+	pca_data <- plotPCA(vst, intgroup = c("group", "Cultivar", "tissue", "Age", "BioProject", "dev_stage"), returnData = TRUE)
+	# Plot customized PCA 
+	percentVar <- round(100 * attr(pca_data, "percentVar"))
+	# plot R color condition
+	colors = wes_palette("Darjeeling1",2 , type = "discrete")
+	# Plooot 
+	p <- ggplot(pca_data, aes(x = PC1, y = PC2, shape = as.factor(Cultivar),  color = as.factor(group.1)))
+#	p <- p + geom_jitter(aes(color = group), size = 3, show.legend = FALSE) +
+#	facet_wrap(~ ConditionDescription)
+	p <- p + geom_point(size=3)
+	p <- p + labs(title = "sorghum", color="Group", shape = "Genotype" )
+	p <- p + xlab((paste0("PC1 : ", percentVar[1],"%")))
+	p <- p + ylab((paste0("PC2 : ", percentVar[2],"%")))
+#	p <- p + geom_text_repel(aes(label=name), size=2.5, max.overlaps = 20 )
+	p <- p + scale_colour_manual(values = colors)
+	p <- p + theme_bw()
+	p <- p + theme( text = element_text(family = "Times New Roman", size=22),
+                panel.border = element_blank(),
+                panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                axis.line = element_line(colour = "black"))
+
+ggsave(p, filename = "../results/pca_sorghum_group.png" ,units = "cm", width = 15*1.3, height = 15, dpi = 320, limitsize = F)
+
+}
+#get_full_gene_count_matrixes("./../data/metadata_complete.csv",  "/Storage/data1/jorge.munoz/good_t2gene/results/tx2gen_DESEQ.csv")
+#get_full_trans_count_matrixes("./../data/metadata_complete.csv",  "/Storage/data1/jorge.munoz/good_t2gene/results/tx2gen_DESEQ.csv")
+
