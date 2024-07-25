8a66886213500e2c886fd573b3d08c60f9dfd1a4
jomare1188 <jomare1188@gmail.com>
2024-07-25 15:19:51 -0300

    adding scripts for preprocessing step

---
+++ code/get_results.sh
@@ -0,0 +1,13 @@
+# all_cds, all_transcripts, longest_cds_per_orthogroup, ref
+# format table accession,reference,mapping_rate
+rm -rf pretty_table.csv
+
+for genotype in $(cat sugarcane_genotypes.txt)
+do
+	results_dir=../data/${genotype}/3_salmon_quant
+	for sample in $(sort -u ids_sra/${genotype}_ids.txt)
+	do
+		mapping_rate=$(grep -w "Mapping rate" ${results_dir}/${sample}/logs/salmon_quant.log | rev | cut -f1 -d" " | rev | sed "s/%//g")
+		echo ${sample},${genotype},${mapping_rate} >> pretty_table.csv
+	done
+done
---
+++ code/plot_quant.r
@@ -0,0 +1,24 @@
+raw <- read.table("pretty_table_sugarcane.csv", col.names = c("Accession", "Genotype", "Mapping"), header = F, sep = ",")
+library(dplyr)
+library(ggplot2)
+
+ggplot(data = raw, aes (x = Genotype, y = Mapping))+
+geom_boxplot()+
+  ggtitle("Sugarcane")+
+  xlab("Genotype") + 
+  ylab("Mapping %") +
+  ylim(0, 100) +
+  theme_bw() +
+  theme( panel.grid.major = element_blank(),
+         axis.text.x = element_text(angle = 45, vjust = 1, hjust=1, size = 14, face="bold"),
+         axis.text.y = element_text(size =14, face="bold"),
+         plot.title = element_text(size=22), 
+         panel.grid.minor = element_blank(),
+         panel.border = element_blank(),
+         panel.background = element_blank(),
+         axis.line = element_line(colour = "black")
+  )
+
+ggsave(filename = "quant_sugarcane.svg", device = "svg", units = "in", width = 6.05*1.2, height = 3.75*1.2)
+ggsave(filename = "quant_sugarcane.png", device = "png", units = "in", width = 6.05*1.2, height = 3.75*1.2)
+
